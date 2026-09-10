ea1fba461bbdd752dc6573f9dd4c8aa8dbf6e2de
Uilian Ries <uilianr@jfrog.com>
2026-09-10 15:32:14 +0200

    Remove condition for old version
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/libcbor/all/conanfile.py
+++ recipes/libcbor/all/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
-from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.53.0"
@@ -59,8 +58,6 @@ class LibCborStackConan(ConanFile):
         tc.variables["CBOR_BUFFER_GROWTH"] = self.options.buffer_growth_factor
         # Relocatable shared libs on macOS
         tc.variables["CMAKE_MACOSX_RPATH"] = True
-        if Version(self.version) < "0.13.0":
-            tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
         tc.generate()
 
     def build(self):
